beb2c92df60ecb5636d16a3c3eb0eb140b67431c
jafardahod <jafardahod7@gmail.com>
2024-08-17 17:15:07 +0530

    add reset game logic add more comments for code readability add main game logic to end the game also add css to make the bad piece red

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //selecting the parent of tiles
 let row = document.querySelector('.mainrow')
-
+let gameClickcounter = 1
 //adding every piece with js
 function addpiece() {
     let piece = document.createElement('div')
-    piece.setAttribute('class', 'col-3 col-sm-3 col-xs-3 brr piece')
+    piece.setAttribute('class', `col-3 col-sm-3 col-xs-3 brr piece`)
     row.appendChild(piece)
 }
 
 
 //random number
-let random = Math.floor((Math.random() * 12) + 1)
+let random = Math.floor((Math.random() * 11) + 1)
 
 for (let i = 0; i < 12; i++) {
     addpiece()
@@ -19,8 +19,25 @@ for (let i = 0; i < 12; i++) {
 // on click piece function
 row.addEventListener('click', (e) => {
     if (e.target.className == 'col-3 col-sm-3 col-xs-3 brr piece') {
-
-        e.target.setAttribute('class', 'col-3 col-sm-3 col-xs-3 brr piececlicked')
+        if (amtjs == 0) {
+            alert('Please Pay 1200rs to Play a the game')
+        }
+        else {
+            if(gameClickcounter == random){
+                console.log(random)
+                gameover(e)
+            }
+            else{
+                
+                if (gameClickcounter < 12) {
+    
+                    gamelogic(e)
+                }
+                else {
+                    alert('Game Over')
+                }
+            }
+        }
     }
 })
 
@@ -55,3 +72,44 @@ addamtbtn.addEventListener('click', (e) => {
 let winnings = document.querySelector('.winnings')
 let winningsjs = 0
 winnings.innerHTML = `Total Winnings: ${winningsjs}`
+
+
+
+//main game logic
+
+function gamelogic(e) {
+
+    e.target.setAttribute('class', 'col-3 col-sm-3 col-xs-3 brr piececlicked')
+    amtjs = amtjs - 100
+    amntremain.innerHTML = `Amount Remaining: ${amtjs}`
+    gameClickcounter++
+    if(gameClickcounter > 0 && 4 > gameClickcounter){
+
+        winningsjs = winningsjs + 150
+        winnings.innerHTML = `Total Winnings: ${winningsjs} <span style="color: lightgreen; fontweight: bold">50%</span>`
+    }
+    else if (gameClickcounter > 3 && 8 > gameClickcounter){
+        
+        winningsjs = winningsjs * 2
+        winnings.innerHTML = `Total Winnings: ${winningsjs} <span style="color: lightgreen; fontweight: bold">200%</span>`
+    }else if(gameClickcounter > 7 && 12 > gameClickcounter){
+        
+        winningsjs = winningsjs * 3
+        winnings.innerHTML = `Total Winnings: ${winningsjs} <span style="color: lightgreen; fontweight: bold">300%</span>`
+    }
+
+}
+
+//game over function if someone clicks red 
+function gameover(e){
+    
+    e.target.setAttribute('class', 'col-3 col-sm-3 col-xs-3 brr piececlickedred')
+    // resetgame()
+    amtjs = 0
+    winningsjs = 0
+    random = 0
+    amntremain.innerHTML = `Amount Remaining: ${amtjs}`
+    winnings.innerHTML = `Total Winnings: ${winningsjs}`
+    gameClickcounter = 0
+    // alert('Sorry Game Over')
+}
